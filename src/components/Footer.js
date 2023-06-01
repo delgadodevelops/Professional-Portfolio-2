1832b048dfe296cc17e3da5d21b51772e2f1b091
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-06-01 19:17:57 -0400

    updated navabr page and styled it with tailwind css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="p-4 bg-gray-800 text-white text-center">
+    <footer className="p-4 text-white text-center">
       &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
     </footer>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,30 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="flex items-center justify-between p-4 bg-gray-800 text-white">
+    <nav className="flex items-center justify-between p-4 text-white">
       <div>
-        <Link to="/" className="font-bold text-lg">My Portfolio</Link>
+        <Link to="/" className="flex items-center">
+          <img
+            src="./images/DAVID-DELGADO-NAV-LOGO-UPDATED.png"
+            alt="Logo"
+            className="h-8 w-auto mr-2" // Adjust the height and width of the logo as needed
+          />
+        </Link>
       </div>
-      <div className="flex space-x-4">
-        <Link to="/about" className="text-white hover:text-gray-400">About</Link>
-        <Link to="/projects" className="text-white hover:text-gray-400">Projects</Link>
-        <Link to="/contact" className="text-white hover:text-gray-400">Contact</Link>
+      <div className="flex space-x-14">
+        <Link to="/about" className="text-white .text-white:hover relative">
+          
+          About
+          <span className="underline-animation"></span>
+        </Link>
+        <Link to="/projects" className="text-white .text-white:hover relative">
+          Projects
+          <span className="underline-animation"></span>
+        </Link>
+        <Link to="/contact" className="text-white .text-white:hover relative">
+          Contact
+          <span className="underline-animation"></span>
+        </Link>
       </div>
     </nav>
   );
