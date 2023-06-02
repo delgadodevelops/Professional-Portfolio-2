58114aa785b52ed38c6c3068f4b5d4661f658982
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-06-02 00:21:41 -0400

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="p-4 text-white text-center mt-10">
-      &copy; {new Date().getFullYear()} My Portfolio. All rights reserved.
+    <footer className="p-4 text-white text-center mt-10 ">
+      <div className="flex justify-center space-x-4">
+        <a href="https://www.linkedin.com/in/david-delgado-02160619a/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="text-white text-xl hover:text-gray-500" />
+        </a>
+        <a href="https://github.com/ddelgado25" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="text-white text-xl hover:text-gray-500" />
+        </a>
+      </div>
+      <p className="mt-4">&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
     </footer>
   );
 };
