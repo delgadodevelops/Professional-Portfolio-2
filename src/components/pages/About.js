c2e3da628c0fed8afa1b37f66c46379fadaf830e
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-06-02 00:32:42 -0400

    made some updates to about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const About = () => {
   const skills = [
@@ -9,12 +9,18 @@ const About = () => {
     { language: 'TailwindCSS', proficiency: 90 },
   ];
 
+  const [animationStarted, setAnimationStarted] = useState(false);
+
+  useEffect(() => {
+    setAnimationStarted(true);
+  }, []);
+
   return (
-    <div className="container mx-auto p-20">
+    <div className="container mx-auto p-20 custom-container">
       <h1 className="text-home text-6xl font-bold mb-5">ABOUT ME //</h1>
       <hr className="text-home border-gray-400 border-t-2 my-6" />
       <div className="mb-8">
-        <h2 className="text-home text-3xl font-bold mb-5">Heres a bit about myself //</h2>
+        <h2 className="text-home text-3xl font-bold mb-5">Here's a bit about myself //</h2>
         <p className="text-white text-xl">
           Hi, my name is David Delgado. I am a recently graduated Full-Stack Developer from the University of Miami Coding Bootcamp. I am fluent in HTML/CSS, JavaScript, mySQL, MongoDB, Express, React, and Node. My real passion comes in the design and UI aspect of applications. I am very well versed in TailwindCSS and have been using it to style my most recent applications, including this one.
         </p>
@@ -24,9 +30,9 @@ const About = () => {
         {skills.map((skill, index) => (
           <div key={index} className="mb-4">
             <h3 className="text-white text-lg font-bold">{skill.language}</h3>
-            <div className="bg-gray-300 h-4 rounded">
+            <div className="bg-gray-300 h-4 rounded overflow-hidden">
               <div
-                className="bg-blue-500 h-4 rounded"
+                className={`bg-blue-500 h-4 rounded ${animationStarted ? 'animate-progress' : ''}`}
                 style={{ width: `${skill.proficiency}%` }}
               ></div>
             </div>
