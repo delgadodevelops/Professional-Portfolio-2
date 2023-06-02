fc5ef22be4f2a3f41b4b9352b6ef402d54c4e496
ddelgado25 <118149533+ddelgado25@users.noreply.github.com>
2023-06-02 00:24:00 -0400

    added email icon to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaEnvelope } from 'react-icons/fa';
 
 const Footer = () => {
+  const handleEmailClick = () => {
+    window.location.href = 'mailto:your-email@example.com';
+  };
+
   return (
     <footer className="p-4 text-white text-center mt-10 ">
       <div className="flex justify-center space-x-4">
-        <a href="https://www.linkedin.com/in/david-delgado-02160619a/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
           <FaLinkedin className="text-white text-xl hover:text-gray-500" />
         </a>
-        <a href="https://github.com/ddelgado25" target="_blank" rel="noopener noreferrer">
+        <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
           <FaGithub className="text-white text-xl hover:text-gray-500" />
+        </a>
+        <a href="#" onClick={handleEmailClick}>
+          <FaEnvelope className="text-white text-xl hover:text-gray-500" />
         </a>
       </div>
       <p className="mt-4">&copy; {new Date().getFullYear()} My Portfolio. All rights reserved.</p>
